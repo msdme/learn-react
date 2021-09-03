db7372e9d6f2786529ff795d5270e7146e1b5499
msdme <masdi@ivosights.com>
2021-09-03 18:28:57 +0700

    useFetch, custom hook, dan rekonstuktur  objek balikan ke Home, contoh, maping data ke blogs

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,9 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
     
-    useEffect( ()=>{
-        fetch('http://localhost:8000/blogs')
-            .then(response => {
-                if(!response.ok){
-                    throw Error('Data Gagal diambil');
-                }
-                return response.json();
-            })
-            // data disini didapat dari then sebelumnya, 
-            // yaitu return response.json();
-            .then((data)=>{
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            //catch ini didapat dari throw error diatas
-            // tapi bisa juga bukan, seperti server mati
-            .catch((err)=>{
-                setIsPending(false)
-                setError(err.message)
-            })
-    },[] );
+    const { data:blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
     return ( 
         <div className="home">
             { error && <div>{ error }</div> }
---
+++ src/useFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+
+// custom hooks di react harus diawali dengan "use", untuk itu hook ini di beri nama "useFetch"
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect( ()=>{
+        fetch(url)
+            .then(response => {
+                if(!response.ok){
+                    throw Error('Data Gagal diambil');
+                }
+                return response.json();
+            })
+            // data disini didapat dari then sebelumnya, 
+            // yaitu return response.json();
+            .then((data)=>{
+                setData(data)
+                setIsPending(false)
+                setError(null)
+            })
+            //catch ini didapat dari throw error diatas
+            // tapi bisa juga bukan, seperti server mati
+            .catch((err)=>{
+                setIsPending(false)
+                setError(err.message)
+            })
+    },[] );
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
