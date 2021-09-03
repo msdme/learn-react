b71597037f845a2eba2223754dcc55c1667eb7f4
msdme <masdi@ivosights.com>
2021-09-03 18:18:51 +0700

    Handle Error ketika api error

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,14 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
-
+    const [error, setError] = useState(null);
     
     useEffect( ()=>{
         fetch('http://localhost:8000/blogs')
             .then(response => {
+                if(!response.ok){
+                    throw Error('Data Gagal diambil');
+                }
                 return response.json();
             })
             // data disini didapat dari then sebelumnya, 
@@ -16,10 +19,18 @@ const Home = () => {
             .then((data)=>{
                 setBlogs(data)
                 setIsPending(false)
+                setError(null)
+            })
+            //catch ini didapat dari throw error diatas
+            // tapi bisa juga bukan, seperti server mati
+            .catch((err)=>{
+                setIsPending(false)
+                setError(err.message)
             })
     },[] );
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
             { isPending && <div>Loading...</div> }
             { blogs && <BlogList blogs={ blogs } title="Blog List"/>}
         </div>
