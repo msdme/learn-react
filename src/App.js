75c1780372ead6a05eb7ce1b9993218e45c72a02
msdme <masdi@ivosights.com>
2021-09-03 17:01:03 +0700

    Lesson 10, terkecoh di penggunaan arrow function, disini menggunakan (, bukan {, jadinya langsung di tampilkan di browser, ini juga syarat penggunaan jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-
 import Navbar from './Navbar';
 import Home from './Home';
 
 
 function App() {
-    const title = "Welcome to the new blog";
-    const likes = 50;
+    
     return (
         <div className="App">
             <Navbar />
